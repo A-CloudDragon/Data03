5dce04da061843b0643c4d2cac8b7487641a4a57
yunhuaifeilong <811353256@qq.com>
2019-06-27 15:59:53 +0800

    这是一个新的项目 刚刚加了set动画进厂

--- app/src/main/java/com/jiyun/data03/MainActivity.java
+++ app/src/main/java/com/jiyun/data03/MainActivity.java
@@ -1,13 +1,45 @@
 package com.jiyun.data03;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.anim_img)
+    ImageView mAnimImg;
+    @BindView(R.id.anim_tv)
+    TextView mAnimTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Animation animation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.s);
+        mAnimImg.startAnimation(animation);
+
+          new CountDownTimer(10000,100){
+
+                      @Override
+                      public void onTick(long millisUntilFinished) {
+                          mAnimTv.setText(millisUntilFinished/1000+"");
+                      }
+
+                      @Override
+                      public void onFinish() {
+                          startActivity(new Intent(MainActivity.this,Main2Activity.class));
+
+                      }
+                  }.start();
     }
 }
